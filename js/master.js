15e8ca61dbae240a9f9618356bb044a0e2798c4e
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-04-22 01:26:43 +0200

    end make functions and handle style

--- js/master.js
+++ js/master.js
@@ -1,79 +1,172 @@
 "use strict";
 
 // Get button element to control on click event
-
 var successBtn = document.querySelector(".sucsess");
 var errorBtn = document.querySelector(".error");
 var warningBTN = document.querySelector(".warning");
-if (successBtn) {
-  successBtn.addEventListener("click", function () {
-    // create element successfully
-    var DivSuccess = document.createElement("div");
-    DivSuccess.className = "success_notification";
-    document.body.appendChild(DivSuccess);
-    /*************************************************/
-    // add active class to transition
-    setTimeout(function () {
-      DivSuccess.classList.add("active");
-    }, 10);
-    /*************************************************/
-    // stop click on btn some seconds
-    setTimeout(function () {
-      successBtn.disabled = true; // Disable the button for 5 seconds
-    }, 0);
-    setTimeout(function () {
-      successBtn.disabled = false; // Re-enable the button after 5 seconds
-    }, 3500);
-    /*************************************************/
-    // remove element form page
-    setTimeout(function () {
-      DivSuccess.remove();
-    }, 3000);
-  });
-}
-if (errorBtn) {
-  errorBtn.addEventListener("click", function () {
-    // create element error
-    var DivError = document.createElement("div");
-    DivError.className = "error_notification";
-    document.body.appendChild(DivError);
-    /*************************************************/
-    // add active class to transition
-    setTimeout(function () {
-      DivError.classList.add("active");
-    }, 10);
-    /*************************************************/
-    // stop click on btn some seconds
-    setTimeout(function () {
-      successBtn.disabled = false; // Re-enable the button after 5 seconds
-    }, 2000);
-    /*************************************************/
-    // remove element form page
-    setTimeout(function () {
-      DivError.remove();
-    }, 2300);
-  });
-}
-if (warningBTN) {
-  warningBTN.addEventListener("click", function () {
-    // create element warning
-    var DivWarning = document.createElement("div");
-    DivWarning.className = "warning_notification";
-    document.body.appendChild(DivWarning);
-    /*************************************************/
-    // add active class to transition
-    setTimeout(function () {
-      DivWarning.classList.add("active");
-    }, 10);
-    /*************************************************/
-    // stop click on btn some seconds
-    setTimeout(function () {
-      successBtn.disabled = false; // Re-enable the button after 5 seconds
-    }, 2000);
-    /*************************************************/
-    // remove element form page
-    setTimeout(function () {
-      DivWarning.remove();
-    }, 2300);
-  });
-}
+var success_alert_Ash = function success_alert_Ash() {
+  if (successBtn) {
+    successBtn.addEventListener("click", function () {
+      // create element successfully
+      var DivSuccess = document.createElement("div");
+      DivSuccess.className = "success_notification";
+      /*************************************************/
+      // btn content
+      var divIconStatus = document.createElement("div");
+      divIconStatus.style.backgroundColor = "#EEE";
+      divIconStatus.style.width = "50px";
+      divIconStatus.style.height = "100%";
+      divIconStatus.style.position = "relative";
+      var content_iconLeft = document.createElement("img");
+      content_iconLeft.src = "../../assets/check.png";
+      content_iconLeft.style.position = "absolute";
+      content_iconLeft.style.width = "100%";
+      content_iconLeft.style.height = "100%";
+      content_iconLeft.style.padding = "10px";
+      divIconStatus.appendChild(content_iconLeft);
+      DivSuccess.appendChild(divIconStatus);
+      /*************************************************/
+      var title = document.createElement("span");
+      title.style.position = "absolute";
+      title.style.top = "50%";
+      title.style.left = "50%";
+      title.style.transform = "translate(-50%, -50%)";
+      title.style.color = "white";
+      title.style.fontSize = "22px";
+      title.style.width = "80%";
+      title.style.textAlign = "center";
+      title.innerHTML = "process successfully 😍";
+      DivSuccess.appendChild(title);
+      document.body.appendChild(DivSuccess);
+      /*************************************************/
+      // add active class to transition
+      setTimeout(function () {
+        DivSuccess.classList.add("active");
+      }, 10);
+      /*************************************************/
+      // stop click on btn some seconds
+      setTimeout(function () {
+        successBtn.disabled = true;
+      }, 0);
+      setTimeout(function () {
+        successBtn.disabled = false;
+      }, 3500);
+      /*************************************************/
+      // remove element form page
+      setTimeout(function () {
+        DivSuccess.remove();
+      }, 3500);
+    });
+  }
+};
+var warning_alert_Ash = function warning_alert_Ash() {
+  if (warningBTN) {
+    warningBTN.addEventListener("click", function () {
+      // create element warning
+      var DivWarning = document.createElement("div");
+      DivWarning.className = "warning_notification";
+      // btn content
+      var divIconStatus = document.createElement("div");
+      divIconStatus.style.backgroundColor = "#EEE";
+      divIconStatus.style.width = "50px";
+      divIconStatus.style.height = "100%";
+      divIconStatus.style.position = "relative";
+      var content_iconLeft = document.createElement("img");
+      content_iconLeft.src = "../../assets/warning.png";
+      content_iconLeft.style.position = "absolute";
+      content_iconLeft.style.width = "100%";
+      content_iconLeft.style.height = "100%";
+      content_iconLeft.style.padding = "10px";
+      divIconStatus.appendChild(content_iconLeft);
+      DivWarning.appendChild(divIconStatus);
+      /*************************************************/
+      var title = document.createElement("span");
+      title.style.position = "absolute";
+      title.style.top = "50%";
+      title.style.left = "50%";
+      title.style.transform = "translate(-50%, -50%)";
+      title.style.color = "white";
+      title.style.fontSize = "22px";
+      title.style.width = "80%";
+      title.style.textAlign = "center";
+      title.innerHTML = "process Warning 🫤";
+      DivWarning.appendChild(title);
+      document.body.appendChild(DivWarning);
+      /*************************************************/
+      // add active class to transition
+      setTimeout(function () {
+        DivWarning.classList.add("active");
+      }, 10);
+      /*************************************************/
+      // stop click on btn some seconds
+      setTimeout(function () {
+        warningBTN.disabled = true;
+      }, 0);
+      setTimeout(function () {
+        warningBTN.disabled = false;
+      }, 3500);
+      /*************************************************/
+      // remove element form page
+      setTimeout(function () {
+        DivWarning.remove();
+      }, 2300);
+    });
+  }
+};
+var error_alert_Ash = function error_alert_Ash() {
+  if (errorBtn) {
+    errorBtn.addEventListener("click", function () {
+      // create element error
+      var DivError = document.createElement("div");
+      DivError.className = "error_notification";
+      // btn content
+      var divIconStatus = document.createElement("div");
+      divIconStatus.style.width = "50px";
+      divIconStatus.style.height = "100%";
+      divIconStatus.style.backgroundColor = "#EEE";
+      divIconStatus.style.position = "relative";
+      var content_iconLeft = document.createElement("img");
+      content_iconLeft.src = "../../assets/error.png";
+      content_iconLeft.style.position = "absolute";
+      content_iconLeft.style.width = "100%";
+      content_iconLeft.style.height = "100%";
+      content_iconLeft.style.padding = "10px";
+      divIconStatus.appendChild(content_iconLeft);
+      DivError.appendChild(divIconStatus);
+      /*************************************************/
+      var title = document.createElement("span");
+      title.style.position = "absolute";
+      title.style.top = "50%";
+      title.style.left = "50%";
+      title.style.transform = "translate(-50%, -50%)";
+      title.style.color = "white";
+      title.style.fontSize = "22px";
+      title.style.width = "80%";
+      title.style.textAlign = "center";
+      title.innerHTML = "process bom 😭";
+      DivError.appendChild(title);
+      document.body.appendChild(DivError);
+      /*************************************************/
+      // add active class to transition
+      setTimeout(function () {
+        DivError.classList.add("active");
+      }, 10);
+      /*************************************************/
+      // stop click on btn some seconds
+      setTimeout(function () {
+        errorBtn.disabled = true;
+      }, 0);
+      setTimeout(function () {
+        errorBtn.disabled = false;
+      }, 3500);
+      /*************************************************/
+      // remove element form page
+      setTimeout(function () {
+        DivError.remove();
+      }, 2500);
+    });
+  }
+};
+success_alert_Ash();
+warning_alert_Ash();
+error_alert_Ash();
--- layout/ts/master.ts
+++ layout/ts/master.ts
@@ -1,80 +1,176 @@
 // Get button element to control on click event
-
 const successBtn = document.querySelector(".sucsess") as HTMLButtonElement;
 const errorBtn = document.querySelector(".error") as HTMLButtonElement;
 const warningBTN = document.querySelector(".warning") as HTMLButtonElement;
 
-if (successBtn) {
-  successBtn.addEventListener("click", () => {
-    // create element successfully
-    const DivSuccess = document.createElement("div");
-    DivSuccess.className = "success_notification";
-    document.body.appendChild(DivSuccess);
-    /*************************************************/
-    // add active class to transition
-    setTimeout(() => {
-      DivSuccess.classList.add("active");
-    }, 10);
-    /*************************************************/
-    // stop click on btn some seconds
-    setTimeout(() => {
-      successBtn.disabled = true; // Disable the button for 5 seconds
-    }, 0);
-    setTimeout(() => {
-      successBtn.disabled = false; // Re-enable the button after 5 seconds
-    }, 3500);
-    /*************************************************/
-    // remove element form page
-    setTimeout(() => {
-      DivSuccess.remove();
-    }, 3000);
-  });
+const success_alert_Ash = () => {
+  if (successBtn) {
+    successBtn.addEventListener("click", () => {
+      // create element successfully
+      const DivSuccess = document.createElement("div");
+      DivSuccess.className = "success_notification";
+      /*************************************************/
+      // btn content
+      const divIconStatus = document.createElement("div");
+      divIconStatus.style.backgroundColor = "#EEE";
+      divIconStatus.style.width = "50px";
+      divIconStatus.style.height = "100%";
+      divIconStatus.style.position = "relative";
+      const content_iconLeft = document.createElement("img");
+      content_iconLeft.src = "../../assets/check.png";
+      content_iconLeft.style.position = "absolute";
+      content_iconLeft.style.width = "100%";
+      content_iconLeft.style.height = "100%";
+      content_iconLeft.style.padding = "10px";
+      divIconStatus.appendChild(content_iconLeft);
+      DivSuccess.appendChild(divIconStatus);
+      /*************************************************/
+      const title = document.createElement("span");
+      title.style.position = "absolute";
+      title.style.top = "50%";
+      title.style.left = "50%";
+      title.style.transform = "translate(-50%, -50%)";
+      title.style.color = "white";
+      title.style.fontSize = "22px";
+      title.style.width = "80%";
+      title.style.textAlign = "center";
+  
+      title.innerHTML = "process successfully 😍";
+      DivSuccess.appendChild(title);
+      document.body.appendChild(DivSuccess);
+      /*************************************************/
+      // add active class to transition
+      setTimeout(() => {
+        DivSuccess.classList.add("active");
+      }, 10);
+      /*************************************************/
+      // stop click on btn some seconds
+      setTimeout(() => {
+        successBtn.disabled = true;
+      }, 0);
+      setTimeout(() => {
+        successBtn.disabled = false;
+      }, 3500);
+      /*************************************************/
+      // remove element form page
+      setTimeout(() => {
+        DivSuccess.remove();
+      }, 3500);
+    });
+  }
+}
+const warning_alert_Ash = () =>{
+  if (warningBTN) {
+    warningBTN.addEventListener("click", () => {
+      // create element warning
+      const DivWarning = document.createElement("div");
+      DivWarning.className = "warning_notification";
+      // btn content
+      const divIconStatus = document.createElement("div");
+      divIconStatus.style.backgroundColor = "#EEE";
+      divIconStatus.style.width = "50px";
+      divIconStatus.style.height = "100%";
+      divIconStatus.style.position = "relative";
+      const content_iconLeft = document.createElement("img");
+      content_iconLeft.src = "../../assets/warning.png";
+      content_iconLeft.style.position = "absolute";
+      content_iconLeft.style.width = "100%";
+      content_iconLeft.style.height = "100%";
+      content_iconLeft.style.padding = "10px";
+      divIconStatus.appendChild(content_iconLeft);
+      DivWarning.appendChild(divIconStatus);
+      /*************************************************/
+      const title = document.createElement("span");
+      title.style.position = "absolute";
+      title.style.top = "50%";
+      title.style.left = "50%";
+      title.style.transform = "translate(-50%, -50%)";
+      title.style.color = "white";
+      title.style.fontSize = "22px";
+      title.style.width = "80%";
+      title.style.textAlign = "center";
+  
+      title.innerHTML = "process Warning 🫤";
+      DivWarning.appendChild(title);
+      document.body.appendChild(DivWarning);
+      /*************************************************/
+      // add active class to transition
+      setTimeout(() => {
+        DivWarning.classList.add("active");
+      }, 10);
+      /*************************************************/
+      // stop click on btn some seconds
+      setTimeout(() => {
+        warningBTN.disabled = true;
+      }, 0);
+      setTimeout(() => {
+        warningBTN.disabled = false;
+      }, 3500);
+      /*************************************************/
+      // remove element form page
+      setTimeout(() => {
+        DivWarning.remove();
+      }, 2300);
+    });
+  }
+  
+}
+const error_alert_Ash = () =>{
+  if (errorBtn) {
+    errorBtn.addEventListener("click", () => {
+      // create element error
+      const DivError = document.createElement("div");
+      DivError.className = "error_notification";
+      // btn content
+      const divIconStatus = document.createElement("div");
+      divIconStatus.style.width = "50px";
+      divIconStatus.style.height = "100%";
+      divIconStatus.style.backgroundColor = "#EEE";
+      divIconStatus.style.position = "relative";
+      const content_iconLeft = document.createElement("img");
+      content_iconLeft.src = "../../assets/error.png";
+      content_iconLeft.style.position = "absolute";
+      content_iconLeft.style.width = "100%";
+      content_iconLeft.style.height = "100%";
+      content_iconLeft.style.padding = "10px";
+      divIconStatus.appendChild(content_iconLeft);
+      DivError.appendChild(divIconStatus);
+      /*************************************************/
+      const title = document.createElement("span");
+      title.style.position = "absolute";
+      title.style.top = "50%";
+      title.style.left = "50%";
+      title.style.transform = "translate(-50%, -50%)";
+      title.style.color = "white";
+      title.style.fontSize = "22px";
+      title.style.width = "80%";
+      title.style.textAlign = "center";
+  
+      title.innerHTML = "process bom 😭";
+      DivError.appendChild(title);
+      document.body.appendChild(DivError);
+      /*************************************************/
+      // add active class to transition
+      setTimeout(() => {
+        DivError.classList.add("active");
+      }, 10);
+      /*************************************************/
+      // stop click on btn some seconds
+      setTimeout(() => {
+        errorBtn.disabled = true;
+      }, 0);
+      setTimeout(() => {
+        errorBtn.disabled = false;
+      }, 3500);
+      /*************************************************/
+      // remove element form page
+      setTimeout(() => {
+        DivError.remove();
+      }, 2500);
+    });
+  }
 }
 
-if (errorBtn) {
-  errorBtn.addEventListener("click", () => {
-    // create element error
-    const DivError = document.createElement("div");
-    DivError.className = "error_notification";
-    document.body.appendChild(DivError);
-    /*************************************************/
-    // add active class to transition
-    setTimeout(() => {
-      DivError.classList.add("active");
-    }, 10);
-    /*************************************************/
-    // stop click on btn some seconds
-    setTimeout(() => {
-      successBtn.disabled = false; // Re-enable the button after 5 seconds
-    }, 2000);
-    /*************************************************/
-    // remove element form page
-    setTimeout(() => {
-      DivError.remove();
-    }, 2300);
-  });
-}
-
-if (warningBTN) {
-  warningBTN.addEventListener("click", () => {
-    // create element warning
-    const DivWarning = document.createElement("div");
-    DivWarning.className = "warning_notification";
-    document.body.appendChild(DivWarning);
-    /*************************************************/
-    // add active class to transition
-    setTimeout(() => {
-      DivWarning.classList.add("active");
-    }, 10);
-    /*************************************************/
-    // stop click on btn some seconds
-    setTimeout(() => {
-      successBtn.disabled = false; // Re-enable the button after 5 seconds
-    }, 2000);
-    /*************************************************/
-    // remove element form page
-    setTimeout(() => {
-      DivWarning.remove();
-    }, 2300);
-  });
-}
+success_alert_Ash();
+warning_alert_Ash();
+error_alert_Ash();
